5b1d2ff754a707cc925576f78549be8b4512d0b4
jholder17 <j.holder17@gmail.com>
2025-06-17 23:24:03 -0400

    start nav bar

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -2,7 +2,11 @@ const Navbar = () => {
   return ( 
     <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
       <div className="max-w-7xl mx-auto">
-        <div className="flex justify-betwen items-center py-5 mx-auto c-space"></div>
+        <div className="flex justify-betwen items-center py-5 mx-auto c-space">
+          <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
+            Joseph
+          </a>
+        </div>
       </div>
     </header>
    );
